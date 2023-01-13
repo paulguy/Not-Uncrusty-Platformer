d123e0fc45b6f480cb12188f1b1df41216765efe
Paul LaMendola <paulguy119@gmail.com>
2023-01-12 22:10:56 -0500

    crawly_mob.gd: Maybe fix walking off ledges.

--- crawly_mob.gd
+++ crawly_mob.gd
@@ -1,5 +1,7 @@
 extends Mob
 
+# TODO: why does it randomly float up on respawn?
+
 enum MovementDirection {
 	UP,
 	DOWN,
@@ -7,23 +9,16 @@ enum MovementDirection {
 	RIGHT
 }
 
-@export var direction : MovementDirection = MovementDirection.RIGHT
+@export var startDirection : MovementDirection = MovementDirection.RIGHT
 @export var clockwise : bool = true
 @export_range(0.0, 9999.0) var speed = 1500.0
-var motion = Vector2.ZERO
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var motion = Vector2.ZERO
 var falling = true
+var direction = startDirection
 
-@onready var sprite = self.get_node("Mob Sprite")
-
-var startDirection = direction
-
-func deactivate():
-	super()
-	motion = Vector2.ZERO
-	falling = true
-	direction = startDirection
-	self.set_sprite()
+@onready var sprite = get_node("Mob Sprite")
 
 func set_sprite():
 	if clockwise:
@@ -33,25 +28,25 @@ func set_sprite():
 				sprite.position = Vector2.ZERO
 				sprite.flip_h = false
 				motion = Vector2.UP * speed
-				self.up_direction = Vector2.LEFT
+				up_direction = Vector2.LEFT
 			MovementDirection.DOWN:
 				sprite.texture.region.position.x = sprite.texture.region.size.x
 				sprite.position = Vector2.ZERO
 				sprite.flip_h = true
 				motion = Vector2.DOWN * speed
-				self.up_direction = Vector2.RIGHT
+				up_direction = Vector2.RIGHT
 			MovementDirection.RIGHT:
 				sprite.texture.region.position.x = 0
 				sprite.position = Vector2(0, -1)
 				sprite.flip_v = false
 				motion = Vector2.RIGHT * speed
-				self.up_direction = Vector2.UP
+				up_direction = Vector2.UP
 			MovementDirection.LEFT:
 				sprite.texture.region.position.x = 0
 				sprite.position = Vector2(0, 1)
 				sprite.flip_v = true
 				motion = Vector2.LEFT * speed
-				self.up_direction = Vector2.DOWN
+				up_direction = Vector2.DOWN
 	else:
 		match direction:
 			MovementDirection.UP:
@@ -59,35 +54,43 @@ func set_sprite():
 				sprite.position = Vector2.ZERO
 				sprite.flip_h = true
 				motion = Vector2.UP * speed
-				self.up_direction = Vector2.RIGHT
+				up_direction = Vector2.RIGHT
 			MovementDirection.DOWN:
 				sprite.texture.region.position.x = sprite.texture.region.size.x
 				sprite.position = Vector2.ZERO
 				sprite.flip_h = false
 				motion = Vector2.DOWN * speed
-				self.up_direction = Vector2.LEFT
+				up_direction = Vector2.LEFT
 			MovementDirection.RIGHT:
 				sprite.texture.region.position.x = 0
 				sprite.position = Vector2(0, 1)
 				sprite.flip_v = true
 				motion = Vector2.RIGHT * speed
-				self.up_direction = Vector2.DOWN
+				up_direction = Vector2.DOWN
 			MovementDirection.LEFT:
 				sprite.texture.region.position.x = 0
 				sprite.position = Vector2(0, -1)
 				sprite.flip_v = false
 				motion = Vector2.LEFT * speed
-				self.up_direction = Vector2.UP
+				up_direction = Vector2.UP
+
+func deactivate():
+	super()
+	falling = true
+	direction = startDirection
+	set_sprite()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# make sure it can crawl along most slopes
+	set_floor_max_angle(deg_to_rad(89))
 	set_sprite()
 
 func calc_velocity(delta) -> Vector2:
 	if falling:
 		return velocity + (Vector2(0, gravity) * delta)
 	else:
-		return (motion + (-self.up_direction * gravity)) * delta
+		return (motion + (-up_direction * gravity)) * delta
 
 func set_falling():
 	falling = true
@@ -95,7 +98,7 @@ func set_falling():
 		direction = MovementDirection.RIGHT
 	else:
 		direction = MovementDirection.LEFT
-	self.set_sprite()
+	set_sprite()
 
 func update_direction(dir_order : bool):
 	if dir_order:
@@ -127,34 +130,44 @@ func update_direction(dir_order : bool):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity = calc_velocity(delta)
-	move_and_slide()
+	# split velocity so movement is in smaller steps ~1.0 each
+	var v = calc_velocity(delta)
+	var vi = v.length() * delta
+	v = v / vi
+	
+	for i in vi:
+		velocity = v
+		move_and_slide()
 
-	for j in get_slide_collision_count():
-		var collision = get_slide_collision(j)
-		var collider = collision.get_collider()
-		if collider is Player:
-			collider.hit(position)
-			move_and_slide()
-			break
+		# detect hitting the player and continue motion
+		for j in get_slide_collision_count():
+			var collision = get_slide_collision(j)
+			var collider = collision.get_collider()
+			if collider is Player:
+				# signal player to not collide with mobs
+				collider.hit(position)
+				# complete the motion
+				move_and_slide()
+				break
 
-	if not falling:
-		if is_on_wall():
-			self.update_direction(false)
-		elif not is_on_floor():
-			if direction == MovementDirection.LEFT or direction == MovementDirection.RIGHT:
-				velocity = Vector2(-velocity.x * 2, 0)
-			else:
-				velocity = Vector2(0, -velocity.y * 2)
+		if not falling:
+			if is_on_wall():
+				update_direction(false)
+			elif not is_on_floor():
+				if direction == MovementDirection.LEFT or direction == MovementDirection.RIGHT:
+					v = Vector2(-v.x * 2, 0)
+				else:
+					v = Vector2(0, -v.y * 2)
 
-			self.update_direction(true)
+				update_direction(true)
 
-			move_and_slide()
-			if not is_on_floor():
-				self.set_falling()
-	else:
-		if is_on_floor():
-			falling = false
+				velocity = v
+				move_and_slide()
+				if not is_on_floor():
+					set_falling()
+		else:
+			if is_on_floor():
+				falling = false
 
 func _on_anim_timer_timeout():
 	if direction == MovementDirection.LEFT or direction == MovementDirection.RIGHT:
