022e54133e05eca983e4311a26bc2451ba7299a7
Paul LaMendola <paulguy119@gmail.com>
2023-04-04 16:21:37 -0400

    Allow things to settle on window size changes.
    Before, things would behave weird and mobs would randomly reset and do
    other odd things when changing the window size, so just put things on
    hold for a moment after the window is resized.

--- Game.gd
+++ Game.gd
@@ -3,21 +3,45 @@ extends Node2D
 const TARGET_WIDTH = 400.0
 const TARGET_HEIGHT = 300.0
 const ASPECT_RATIO = TARGET_WIDTH / TARGET_HEIGHT
+const RESETTLE_TIME = 0.5
 
+@onready var player = self.get_node("Player")
 @onready var camera = self.get_node("Player/Player Camera")
 @onready var box = self.get_node("ScreenBox")
 @onready var bg = self.get_node("Background")
 @onready var area = self.get_node("Screen Area")
+var lastSize = Vector2.ZERO
+var lastTime = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	lastSize = get_viewport_rect().size
 	# bg is not needed, but just leave it around in case i want to add it back later, but remove it
 	# from the tree so it doesn't do anything weird.
 	self.remove_child(bg)
 
+func pause(paused : bool):
+	if paused:
+		player.process_mode = PROCESS_MODE_DISABLED
+	else:
+		player.process_mode = PROCESS_MODE_INHERIT
+
+	area.pause_mobs(paused)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	var size = get_viewport_rect().size
+	if size != lastSize:
+		if lastTime < 0:
+			pause(true)
+			lastTime = RESETTLE_TIME
+
+	if lastTime >= 0:
+		lastTime -= _delta
+		if lastTime < 0:
+			pause(false)
+			lastSize = size
+
 	var off = -get_viewport_transform().origin
 	var zoom = 1.0
 	if size.x / size.y > ASPECT_RATIO:
--- Screen Area.gd
+++ Screen Area.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var onscreenMobs = []
 var toBeOnScreen = []
+var mobs_paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,11 +26,22 @@ func _on_area_exited(area : Area2D):
 	area.get_parent().off_screen(onscreenMobs)
 
 func _on_body_entered(body):
-	if body is Mob:
-		onscreenMobs.append(body)
-		body.on_screen()
+	if not mobs_paused:
+		if body is Mob:
+			onscreenMobs.append(body)
+			body.on_screen()
 
 func _on_body_exited(body):
-	if body is Mob:
-		body.off_screen()
-		onscreenMobs.erase(body)
+	if not mobs_paused:
+		if body is Mob:
+			body.off_screen()
+			onscreenMobs.erase(body)
+
+func pause_mobs(paused : bool):
+	mobs_paused = paused
+
+	for item in onscreenMobs:
+		if paused:
+			item.process_mode = PROCESS_MODE_DISABLED
+		else:
+			item.process_mode = PROCESS_MODE_INHERIT
