68dcdf6a250d7cc8b167c76b610f0902ba3ceb92
Paul LaMendola <paulguy119@gmail.com>
2023-04-05 18:35:06 -0400

    More worm stuff.
    This is probably not even its final form.  it still looks pretty ugly
    going along slopes.

--- worm.gd
+++ worm.gd
@@ -35,9 +35,9 @@ func pause():
 
 func play():
 	if direction == MovementDirection.LEFT:
-		anim.position.x = 0
+		anim.offset.x = -1.5
 	else:
-		anim.position.x = -16
+		anim.offset.x = -30.5
 	var animName = _dirToName[direction]
 	if animForward:
 		anim.speed_scale = 1.0
@@ -73,6 +73,10 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		var finalAngle = PI / 2
+		if direction == MovementDirection.LEFT:
+			finalAngle = -PI / 2
+		anim.rotation = move_toward(anim.rotation, finalAngle, PI / 2 * delta * 2)
 		pause()
 	else:
 		unpause()
